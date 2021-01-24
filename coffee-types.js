6990ec7f99a087ece646f6edf45fa0b193da4ab0
Lina Sanabria <linamsp19@gmail.com>
2021-01-24 18:26:50 -0500

    Bug fixed. Everything works

--- coffee-types.js
+++ coffee-types.js
@@ -1,11 +1,10 @@
 function coffee(ingredient1, ingredient2, ingredient3, ingredient4) {
     if (ingredient1 === "Coffee" && ingredient2 === "Coffee" ) {
         console.log("Espresso");
+        
     } else {
         if (ingredient1 === "Espresso" && ingredient2 === "Water") {
             console.log("Americano");
-        } else if (ingredient1 === "Espresso" && ingredient2 === "Steamed Milk") {
-            console.log("Flat white");
         } else if (ingredient1 === "Espresso" && ingredient2 === "Steamed Milk" && ingredient3 === "Milk Foam") {
             console.log("Latte");
         } else if (ingredient1 === "Espresso" && ingredient2 === "Steamed Milk" && ingredient3 === "Cream") {
@@ -14,16 +13,18 @@ function coffee(ingredient1, ingredient2, ingredient3, ingredient4) {
             console.log("Glace");
         } else if (ingredient1 === "Chocolate" && ingredient2 === "Espresso" && ingredient3 === "Steamed Milk" && ingredient4 === "Cream") {
             console.log("Mocha");
+        } else if (ingredient1 === "Espresso" && ingredient2 === "Steamed Milk") {
+            console.log("Flat white");
         } else {
-            console.log("O");
+            console.log("Other type of coffee");
         }
     }
 }
 
 coffee("Coffee", "Coffee");
 coffee("Espresso", "Water");
-coffee("Espresso", "Steamed Milk");
 coffee("Espresso", "Steamed Milk", "Milk Foam");
 coffee("Espresso", "Steamed Milk", "Cream");
 coffee("Espresso", "Ice Cream", "Grated Chocolate");
 coffee("Chocolate", "Espresso", "Steamed Milk", "Cream");
+coffee("Espresso", "Steamed Milk");
